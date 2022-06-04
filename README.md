1841be32d7a8f8d74108eff6ad15327b3d84bec0
unknown <humberto.vaco_24@hotmail.com>
2022-06-04 15:45:12 -0600

    Small change

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 1. Clone this repo
 2. Run `npm install`
-3. Run `npm start`
+3. Enter the details for your own databse in `server.js`
+4. Run `npm start`
+
+** Make sure you use postgreSQL for this code base.
--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME
+    user : '',  // Fill the quotes with the information of your postgresql database 
+    password : '',  // Fill the quotes with the information of your postgresql database
+    database : ''  // Fill the quotes with the information of your postgresql database
   }
 });
 
